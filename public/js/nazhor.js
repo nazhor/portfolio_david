4417ad084412cc80d68571f62f7e888762af1be5
Nazhor <dbernad4220@gmail.com>
2019-08-19 20:06:33 +0200

    About page layout

---
+++ public/js/nazhor.js
@@ -0,0 +1,15 @@
+// $('#page-id').hover(
+//     function() {
+//         $("#header-container").addClass("header-hover");
+//         console.log("entro");
+//     }, function() {
+//         $("#header-container").removeClass("header-hover");
+//         console.log("salgo");
+//     }
+// );
+
+$('#page-button').click(
+    function() {
+        console.log("prueba");
+    }
+);
